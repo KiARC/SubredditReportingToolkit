4060ff63841c4d51ba6685209a572aa610fcfbea
HexBliss <73847484+HexBliss@users.noreply.github.com>
2021-04-26 15:08:42 -0400

    Update reportSubs.py
    Updating it with some QoL stuff so you can see the message before sending it.

--- reportSubs.py
+++ reportSubs.py
@@ -5,32 +5,37 @@ import SubredditTester
 reddit = praw.Reddit(
     client_id="<client-id>",
     client_secret="<client-secret>",
-    user_agent="python:subtester:0.1 (by /u/ACEDT)",
+    user_agent="python:subtester:0.2 (by /u/ACEDT)",
     password="<your-password>",
     username="<your-username>",
 )
 
-def reportSubs(subsActive, subsReporting):
+def reportSubs(subsActive, subsPrivate, subsReporting):
     reason = input("Please briefly explain why you are reporting these subreddits: ")
+    message = "I am sending this message to report the following subreddit(s) for the following reason: " + reason + " \n\n" + '\n'.join([i for i in subsActive[0:]]) + "\n" + '\n'.join([i for i in subsPrivate[0:]]) + "\nThank you for looking into this. \n\n^(This message was sent using PRAW but I do manually add subreddits to the list. I use this script to automatically report the list of subreddits I have compiled so that I can do so all at once rather than sending many individual reports.)"
+    print("Here is the report message: \n\n" + message + "\n")
     confirm = input("You are reporting " + str(subsReporting) + " subreddit(s) for \"" + reason + "\". Continue? (y/N): ")
     if confirm == "y":
         print("Okay, sending report message...")
     else:
         print("Cancelling report.")
         quit()
-    message = "I am sending this message to report the following subreddit(s) for the following reason: " + reason + " \n\n" + '\n'.join([i for i in subsActive[0:]]) + "\nThank you for looking into this. \n\n^(This message was sent using PRAW but I do manually add subreddits to the list. I use this script to automatically report the list of subreddits I have compiled so that I can do so all at once rather than sending many individual reports.)"
     reddit.subreddit("reddit.com").message("Subreddit Report", message)
     reddit.redditor(username).message("Copy of Subreddit Report", message)
 def runner():
     print("Obtaining list of active subs from the list...")
-    subsActive = SubredditTester.runner(False)
+    subsActive, subsPrivate = SubredditTester.runner(False)
     if len(subsActive) == 0:
         print("No active subreddits found!")
         quit()
-    subsReporting = len(subsActive)
-    print(str(subsReporting) + " active subreddit(s) found:\n" + 20*"=")
+    subsReporting = len(subsActive) + len(subsPrivate)
+    print("\n" + str(len(subsActive)) + " active subreddit(s) found:\n\n" + 20*"=" + "\n")
     for sub in subsActive:
         print(sub)
     print(20*"=")
-    reportSubs(subsActive, subsReporting)
+    print("\n" + str(len(subsPrivate)) + " private subreddit(s) found:\n\n" + 20*"=" + "\n")
+    for sub in subsPrivate:
+        print(sub)
+    print(20*"=")
+    reportSubs(subsActive, subsPrivate, subsReporting)
 runner()
