734469733cdac31bc457d049c225166b04ecf52f
HexBliss <73847484+HexBliss@users.noreply.github.com>
2021-06-11 14:40:55 -0400

    Changed the way results display

--- SubredditTester.py
+++ SubredditTester.py
@@ -1,13 +1,4 @@
-import praw
-import prawcore
-
-reddit = praw.Reddit(
-    client_id="<client-id>",
-    client_secret="<client-secret>",
-    user_agent="python:subtester:0.2 (by /u/ACEDT)",
-)
-
-def subExists(sub):
+def subTest(sub):
     exists = 0
     try:
         subtest = reddit.subreddit(sub)
@@ -25,7 +16,7 @@ def readList(inFilePath):
         for line in infile:
             line = line.replace("\n","")
             line = line.replace("r/","",1)
-            line = line.split(' !', 1)[0] #Allows use of ! to add a comment in the sublist for readability
+            line = line.split(' !', 1)[0] #Allows use of ! to add a comment in the sublist for context, such as explaining why a sub is in the list or what it is
             subs.append(line)
     return subs
 def outputResults(subs, printSubs):
@@ -35,27 +26,40 @@ def outputResults(subs, printSubs):
     subsMissing = []
     for sub in subs:
         sub = sub.lower()
-        status = subExists(sub)
+        status = subTest(sub)
         if status == 0:
-            if printSubs:
-                print("Subreddit " + sub + " is active.")
             subsActive.append(sub)
         elif status == 1:
-            if printSubs:
-                print("Subreddit " + sub + " does not exist.")
             subsMissing.append(sub)
         elif status == 2:
-            if printSubs:
-                print("Subreddit " + sub + " has been banned.")
             subsBanned.append(sub)
         elif status == 3:
-            if printSubs:
-                print("Subreddit " + sub + " has been made private.")
             subsPrivate.append(sub)
-    subsActive = ["r/" + sub + "\n" for sub in subsActive]
-    subsPrivate = ["r/" + sub + "\n" for sub in subsPrivate]
-    subsBanned = ["r/" + sub + "\n" for sub in subsBanned]
-    subsMissing = ["r/" + sub + "\n" for sub in subsMissing]
+    subsActive = ["r/" + sub for sub in subsActive]
+    subsPrivate = ["r/" + sub for sub in subsPrivate]
+    subsBanned = ["r/" + sub for sub in subsBanned]
+    subsMissing = ["r/" + sub for sub in subsMissing]
+    if printSubs:
+        if len(subsActive) > 0:
+            print("Active subreddits:")
+            for sub in subsActive:
+                print("\t" + sub)
+        if len(subsPrivate) > 0:
+            print("Private subreddits:")
+            for sub in subsPrivate:
+                print("\t" + sub)
+        if len(subsBanned) > 0:
+            print("Banned subreddits:")
+            for sub in subsBanned:
+                print("\t" + sub)
+        if len(subsMissing) > 0:
+            print("Missing subreddits (possibly typos):")
+            for sub in subsMissing:
+                print("\t" + sub)
+    subsActive = [sub + "\n" for sub in subsActive]
+    subsPrivate = [sub + "\n" for sub in subsPrivate]
+    subsBanned = [sub + "\n" for sub in subsBanned]
+    subsMissing = [sub + "\n" for sub in subsMissing]
     return subsActive, subsPrivate, subsBanned, subsMissing
 def logData(logFilePath, subsActive, subsPrivate, subsBanned, subsMissing):
     with open(logFilePath,"w") as logFile:
